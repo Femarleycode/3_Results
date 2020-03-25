d628aed45c6d2c5d122ed507f0f506a44d2c2975
femarleycode <femarleycode@gmail.com>
2020-03-25 13:05:11 +0000

    3rd commit

--- src/examMarks/CallRunner.java
+++ src/examMarks/CallRunner.java
@@ -4,9 +4,9 @@ public class CallRunner {
 	public static void main(String[] args) {
 
 		// input from user/student
-		double physicsInput = (95);
-		double chemistryInput = (90);
-		double biologyInput = (90);
+		double physicsInput = (140);
+		double chemistryInput = (85);
+		double biologyInput = (140);
 
 		Results callResults = new Results();
 		callResults.printResults(physicsInput, chemistryInput, biologyInput);
--- src/examMarks/Results.java
+++ src/examMarks/Results.java
@@ -9,10 +9,45 @@ public class Results {
 				+ biology + "\nTotal: " + total);
 	}
 
+	// Percentages
 	public void printMark(double physics, double chemistry, double biology) {
 
 		double percent = (physics + chemistry + biology) * 100 / 450;
-		System.out.println("Your percentage is: \t" + percent);
+		double physPercent = (physics * 100) / 150;
+		double chemPercent = (chemistry * 100) / 150;
+		double biolPercent = (biology * 100) / 150;
+		int failCounter = 0;
+
+		// PHYS
+		if (physics >= 90) {
+			System.out.println(" Physics - Pass \t" + physPercent);
+		} else {
+			System.out.println(" Physics - Fail \t" + physPercent);
+			failCounter++;
+		}
+
+		// CHEM
+		if (chemistry >= 90) {
+			System.out.println(" Chemistry - Pass \t" + chemPercent);
+		} else {
+			System.out.println(" Chemistry - Fail \t" + chemPercent);
+			failCounter++;
+		}
+
+		// BIO
+		if (biology >= 90) {
+			System.out.println(" Biology - Pass \t" + biolPercent);
+		} else {
+			System.out.println(" Biology - Fail \t" + biolPercent);
+			failCounter++;
+		}
+
+		// Overall %
+		System.out.println("Overall percentage: \t" + percent);
+		if (failCounter > 0) {
+			System.out.println("You have failed overall due to failing " + failCounter + " exam(s), sorry.");
+		}
 
 	}
+
 }
